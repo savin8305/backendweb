89d9c68700b702a5e4d0cf4ec589172151e20edf
kanha-9770 <app3.nessco@gmail.com>
2024-06-17 18:38:27 +0530

    all done

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/content/:lang', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT =3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
